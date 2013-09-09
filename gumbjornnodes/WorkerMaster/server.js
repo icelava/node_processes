39147061002384f7bb0eaa2918347e412bd209ae
Aaron Seet <technology@icelava.net>
2013-09-09 23:51:32 +0800

    Determine number of cpus in system.

--- gumbjornnodes/WorkerMaster/server.js
+++ gumbjornnodes/WorkerMaster/server.js
@@ -1,6 +1,27 @@
-var http = require('http');
-var port = process.env.port || 1337;
+var http = require('http'),
+    os = require('os'),
+    util = require('util');
+
+var port = process.env.port || 1337,
+    availableCpus = os.cpus();
+
+
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<p>' + availableCpus.length + ' cpus</p>')
+    res.write('</body>');
+    res.end('</html>');
 }).listen(port);
+
+availableCpus.forEach(function (cpu) {
+    util.log('new server for cpu', cpu);
+
+    var cpuString = util.inspect(cpu, { showHidden: true, depth: null, colors: true });
+
+    http.createServer(function (req, res) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(cpuString);
+    }).listen(++ port);
+});
